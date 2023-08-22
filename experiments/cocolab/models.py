41464254c8b84765aef402d4a714cbebc51d41a2
emilybunn <99107355+emilybunn@users.noreply.github.com>
2023-08-22 09:13:22 -0700

    Add files via upload
    Different data for each node!

--- experiments/cocolab/models.py
+++ experiments/cocolab/models.py
@@ -9,33 +9,36 @@ class BanditParamsSource(Source):
 
     __mapper_args__ = {"polymorphic_identity": "bandit_params_source"}
 
-    def __init__(self, task_id, network, participant=None):
+    def __init__(self, cond_num, network, participant=None):
         """Endow the source with some persistent properties."""
         super(BanditParamsSource, self).__init__(network=network, participant=participant)
-        self.task_id = task_id
+        self.cond_num = cond_num
 
     @hybrid_property
-    def task_id(self):
+    def cond_num(self):
         """Make property1 condition."""
         return int(self.property1)
 
-    @task_id.setter
-    def task_id(self, condition):
+    @cond_num.setter
+    def cond_num(self, condition):
         """Make condition settable."""
         self.property1 = repr(condition)
 
-    @task_id.expression
-    def task_id(self):
+    @cond_num.expression
+    def cond_num(self):
         """Make condition queryable."""
         return cast(self.property1, Integer)
 
     def _contents(self):
         """Return the relevant task parameters"""
-        print(f'retrieving task id {self.task_id}')
+        print(f'retrieving task id {self.cond_num}')
         with open("static/stimuli/bandit_params.json", "r") as f:
             params = json.load(f)
-            return json.dumps(params[self.task_id])
-
+        with open(f"static/stimuli/teacher_data_{self.cond_num}.json", "r") as f:
+        # with open(f"static/stimuli/teacher_data.json", "r") as f:
+            teacher_data = json.load(f)
+        return json.dumps({"params": params, "teacher_data": teacher_data})
+    
 
 class SSLBanditNetwork(Empty):
 
